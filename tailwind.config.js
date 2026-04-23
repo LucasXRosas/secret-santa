b7819276b145a1c1b5cfa4e50b84ae74a3891099
LucasXRosa <lucasrosa300807@gmail.com>
2026-04-23 14:49:35 -0300

    Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
-  content: [
-    "src/**/*.{html,ts}",
-    "components/**/*.{html,ts}",
-  ],
-    theme: {
-      extend: {
-        colors: {
-          primary: '#D6C2FF',
-          secondary: '#FFD700',
-          accent: '#A7FFB5',
-        },
+  content: ['src/**/*.{html,ts}', 'components/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D6C2FF',
+        secondary: '#FFD700',
+        accent: '#A7FFB5',
       },
-    },
-  extend: {
       fontFamily: {
-        headline: ["'Space Grotesk'", "sans-serif"],
-        body: ["'Public Sans'", "sans-serif"],
+        headline: ['Space Grotesk', 'sans-serif'],
+        body: ['Public Sans', 'sans-serif'],
       },
-      borderRadius: {
-        brand: "0px",
-      }
     },
+  },
+  extend: {
+    borderRadius: {
+      brand: '0px',
+    },
+  },
   plugins: [],
-}
+};
